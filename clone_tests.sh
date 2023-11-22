3c50ff9eea26ab9c52e022801801f1572fdef1fd
KurtJD <kurtisdinelle@gmail.com>
2023-11-22 15:31:29 -0800

    Fix close_tests.sh

--- clone_tests.sh
+++ clone_tests.sh
@@ -3,6 +3,7 @@
 git clone --filter=blob:none --sparse https://github.com/TomHarte/ProcessorTests tests
 cd tests
 git sparse-checkout add 6502/v1
-mv 6502/v1/* .
+mv 6502/v1/*.json .
 rm -rf 6502
 rm README.md
+rm .gitignore
